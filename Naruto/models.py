3feaaf11578e8952bc3fd12dd215db88596abcb9
RomiPadilla <rom.padilla@alumnos.duoc.cl>
2020-11-07 20:57:44 -0300

    otro commit

--- Naruto/models.py
+++ Naruto/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+import uuid # Required for unique book instances
+
 
 # Create your models here.
+
+class Informacione(models.Model):
+	"""Model representing an author."""
+	Nombre = models.CharField(max_length=100)
+	Apellido = models.CharField(max_length=100)
+	imagen = models.ImageField(upload_to='images')
+
+	class Meta:
+		ordering = ['Apellido', 'Nombre']
+
+	def get_absolute_url(self):
+		return reverse('author-detail', args=[str(self.id)])
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return f'{self.Nombre} {self.Apellido}'	
--- Naruto/views.py
+++ Naruto/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Informacione
+from django.views import generic
 # Create your views here.
+
+class InformacioneListView(generic.ListView):
+    model = Informacione
+    template_name='Naruto.html'
+    context_object_name='Informacione_list'
+        
